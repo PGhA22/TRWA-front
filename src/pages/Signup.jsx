4e1a789de6726bf857e37c5d053201c34d476a63
Parsa <instagram18pgh@gmail.com>
2026-02-18 01:37:49 +0330

    change singup number add '+' & fix birthSvg Fill-red

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -162,12 +162,12 @@ function Signup() {
     const dob = parseGermanDate(birthDate);
     if (!dob) {
       document.querySelector("#errorBirth").classList.remove("hidden");
-      document.querySelector("#svgBirth").classList.add("text-red-500");
+      document.querySelector("#svgBirth").classList.add("fill-red-500");
       setLoading(false);
       return;
     } else {
       document.querySelector("#errorBirth").classList.add("hidden");
-      document.querySelector("#svgBirth").classList.remove("text-red-500");
+      document.querySelector("#svgBirth").classList.remove("fill-red-500");
     }
 
     // fea
@@ -178,7 +178,7 @@ function Signup() {
     if (d > t) {
       showError("Geburtsdatum darf nicht in der Zukunft liegen.");
       document.querySelector("#errorBirth").classList.remove("hidden");
-      document.querySelector("#svgBirth").classList.add("text-red-500");
+      document.querySelector("#svgBirth").classList.add("fill-red-500");
       setLoading(false);
       return;
     }
@@ -301,16 +301,8 @@ function Signup() {
             Vollständigen Namen eingeben
           </span>
         </div>
-        <FormInput
-          parentClassName="w-10/12 max-w-md"
-          wrapperClassName="w-10/12 max-w-md my-1.5"
-          inputProps={{
-            type: "text",
-            placeholder: "Mobilnummer",
-            onChange: (e) => setPhone(e.target.value),
-          }}
-          inputClassName="pl-8"
-          icon={
+        <div className="w-10/12 max-w-md my-1.5 relative">
+          <div className="in-glass-border-ch glass-border-ch in-gradient-border gradient-border rounded-4xl relative">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -318,7 +310,7 @@ function Signup() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="size-4.5 z-10 absolute mx-2.5"
+              className="size-4.5 absolute left-3 top-1/2 -translate-y-1/2"
             >
               <path
                 strokeLinecap="round"
@@ -326,10 +318,27 @@ function Signup() {
                 d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
               />
             </svg>
-          }
-          errorId="errorPhone"
-          errorText="Ungültige Mobilnummer"
-        />
+
+            <span className="absolute left-10 top-2/5 -translate-y-2/5 text-white/70">
+              +
+            </span>
+            <input
+              type="tel"
+              inputMode="numeric"
+              placeholder="Mobilnummer"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value.replace(/\D/g, ""))}
+              className="w-full pl-14 pr-3 py-3 rounded-4xl glass-input text-white outline-none"
+            />
+          </div>
+          <span
+            id="errorPhone"
+            className="hidden text-[#ff0000] text-sm mt-1 absolute left-5"
+          >
+            Ungültige Mobilnummer
+          </span>
+        </div>
+
         <FormInput
           parentClassName="w-10/12 max-w-md"
           wrapperClassName="w-10/12 max-w-md my-1.5"
@@ -388,7 +397,7 @@ function Signup() {
             </svg>
           }
           errorId="errorBirth"
-          errorText="Dein Geburtsdatum ist ungültig. Bitte gib dein Geburtsdatum im Format TT.MM.JJJJ ein."
+          errorText="Ungültiges Datum. Format: TT.MM.JJJJ"
           errorClassName=""
         />
 
