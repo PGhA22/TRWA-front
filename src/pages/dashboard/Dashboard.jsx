b7fb211ae05cdd960acce4a5762a572137e8eece
Parsa <instagram18pgh@gmail.com>
2026-02-19 09:58:36 +0330

    create defualt carrousel

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import CarouselDashboard from "../../components/common/CarouselDashboard";
 import "../../style.css";
 import { http } from "../../server/http";
 
@@ -9,9 +10,12 @@ function Dashboard() {
   //   document.title = "Dashboard";
   // }, []);
 
+  const SLIDES = Array.from(Array(3).keys())
+
   return (
     <>
       <div className="text-base h-screen flex flex-col gap-4 text-white font-semibold text-center">
+    <CarouselDashboard slides={SLIDES} options={{ loop: true }} />
         <div className="in-gradient-border gradient-border h-99/100 w-1/5 mt-px py-3 px-0.5 flex flex-col gap-4 bg-blue-50/4 rounded-4xl left-1 ">
           <Link className="relative z-30 block" to="/Support">support</Link>
         </div>
