ea6df95e868b99fe5daf2cc9e38b733ac4527a64
Parsa <instagram18pgh@gmail.com>
2026-02-20 12:30:41 +0330

    carousel v1

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -10,14 +10,23 @@ function Dashboard() {
   //   document.title = "Dashboard";
   // }, []);
 
-  const SLIDES = Array.from(Array(3).keys())
+  const SLIDES = Array.from(Array(3).keys());
 
   return (
     <>
-      <div className="text-base h-screen flex flex-col gap-4 text-white font-semibold text-center">
-        <HeroStackSlider /> 
+      <div className="w-[89.8%] mx-auto text-base h-screen flex flex-col gap-4 text-white font-semibold">
+        <div className="box-dashboard mt-23 pb-4 pt-1">
+          <h5 className="p-6 pl-[5%]">Select your goal to progress forward</h5>
+          <div className="box-dashboard w-9/10 mx-auto overflow-hidden flex justify-center">
+            <div className="w-[740px] flex justify-center">
+              <HeroStackSlider />
+            </div>
+          </div>
+        </div>
         <div className="in-gradient-border gradient-border h-99/100 w-1/5 mt-px py-3 px-0.5 flex flex-col gap-4 bg-blue-50/4 rounded-4xl left-1 ">
-          <Link className="relative z-30 block" to="/Support">support</Link>
+          <Link className="relative z-30 block" to="/Support">
+            support
+          </Link>
         </div>
       </div>
     </>
